cdd08a8b083452ef165fad0fe471efded4b77be2
Aaron Turner <aaron@aaronthedev.com>
2016-02-19 14:44:39 -0800

    Class is over
    Got the game embedded and learned you cannot make a folder for templat types and things :(

---
+++ wp-content/themes/YeoPress/page-bushy-tailed-terror.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+ahdlkjASHFJKHSJKFhklSJDHFLKJSHDLFKjhSLDKJFHKL
+<div id="page-content"
+class = "animated fadeIn">
+	<?php get_template_part('loop', 'page'); ?>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
