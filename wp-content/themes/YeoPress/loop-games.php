828f6a82189eeb625df7196f3d5c09e154f7e465
Aaron Turner <aaron@aaronthedev.com>
2016-02-26 13:51:29 -0800

    Create a loop specifically for games
    Created a loop that specifically only returns our games. I'm stoked!

---
+++ wp-content/themes/YeoPress/loop-games.php
@@ -0,0 +1,49 @@
+<!--Loop to return only published games to us -->
+
+<!-- Create our games query -->
+<?php
+	//Query for our custom posts
+	$customPosts = new WP_Query( array(
+		'post_type' => array('game')
+	));
+	?>
+
+<!-- Loop! -->
+<?php if ($customPosts -> have_posts()):
+		while ($customPosts -> have_posts()) : $customPosts -> the_post() ?>
+
+		<!-- Show the game content -->
+		<article id="article-<?php the_ID() ?>" class="article">
+			<header class="article-header">
+				<h1 class="article-title center">
+
+					<a href="<?php the_permalink() ?>"
+						title="<?php the_title_attribute() ?>">
+							<?php the_title() ?>
+					</a>
+
+				</h1>
+				<div class="article-info center">
+					<span class="date"><?php the_date('m-d-Y') ?></span>
+				</div>
+
+				<?php if (has_post_thumbnail()): ?>
+					<div class="featured-image center">
+						<a href="<?php the_permalink() ?>" title="<?php the_title_attribute() ?>"><?php the_post_thumbnail() ?></a>
+					</div>
+				<?php endif; ?>
+			</header>
+			<div class="article-content">
+				<?php the_content()?>
+			</div>
+		</article>
+
+		<!-- <iframe src="/cecs300/BushyWeb" scrolling="no" class="unityGame">
+		</iframe> -->
+	<hr />
+
+	<?php endwhile; ?>
+
+<?php else: ?>
+	<p>Nothing matches your query.</p>
+<?php  endif; ?>
--- wp-content/themes/YeoPress/page-games.php
+++ wp-content/themes/YeoPress/page-games.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <div id="page-content"
 class = "animated fadeIn">
-	<?php get_template_part('loop', 'page'); ?>
+	<?php get_template_part('loop', 'games'); ?>
 </div>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
