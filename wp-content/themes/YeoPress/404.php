b048c455dc5a20b8bb2e4b1d82e4debb8b3f6577
Aaron Turner <aaron@aaronthedev.com>
2016-03-04 14:18:33 -0800

    Finished up CSS and Added a social network page
    For easy social network adding and things

--- wp-content/themes/YeoPress/404.php
+++ wp-content/themes/YeoPress/404.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
 	<div id="page-content">
-		<h1>Error 404. Try blowing the cartridge?</h1>
-		<?php get_search_form(); ?>
+		<h1 class = "error">Error 404. Try blowing the cartridge?</h1>
+
+		<!-- funny image -->
+		<div class = "errorImage">
+			<img src="https://upload.wikimedia.org/wikipedia/commons/4/48/SNES-SFAM-Cartridges.jpg"
+			alt="Cartridge">
+		</div>
 	</div>
 	<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- wp-content/themes/YeoPress/page-home.php
+++ wp-content/themes/YeoPress/page-home.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
-<div id="page-content">
+<div id="page-content"
+class = "animated fadeIn">
 	<?php get_template_part('loop', 'home'); ?>
 </div>
 <?php get_sidebar(); ?>
